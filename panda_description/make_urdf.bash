1badde016191734955e7be6c3cf8d821152d2214
Tim Player <timrplayer@gmail.com>
2022-01-22 11:44:34 -0800

    Remove misplaced '

--- panda_description/make_urdf.bash
+++ panda_description/make_urdf.bash
@@ -1,4 +1,4 @@
 # run this from panda_description/ folder to 
 # generate urdf
 
-rosrun xacro xacro --inorder urdf/panda_xacro/panda_arm_hand.urdf.xacro > urdf/panda.urdf'
+rosrun xacro xacro --inorder urdf/panda_xacro/panda_arm_hand.urdf.xacro > urdf/panda.urdf
